994107fdb8e3d7cd3182902de3cc59528ecd9a54
sergeyelfimov1 <sergey.elfimov@fdiba.tu-sofia.bg>
2020-11-10 23:21:38 +0000

    .

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -13,10 +13,31 @@ public class Triangle {
             printLine(1, line);
         }
 
+        for (int Sergey = n - 1; Sergey >= 1; Sergey--) {
+          printLine(1, Sergey);
+        }
+
         // Print the bottom part of the triangle that is under the longest line
     }
 
     public static void printLine(int start, int end) {
         // Implement method
+
+        for (int i = start; i <= end; ++i) {
+          System.out.print(i);
+
+          if (i < end) {
+            System.out.print(" ");
+
+          }
+
+          if (i == end) {
+            System.out.println();
+          }
+
+        }
+
+
+
     }
 }
